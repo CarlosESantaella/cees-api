797e4a9ab25d1257107d785a57d3301d725912bd
Hildegar Medina <medinahildegar1@gmail.com>
2024-01-07 11:35:40 -0300

    Migration receptions

---
+++ database/migrations/2024_01_07_142325_create_receptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('equipment_type')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('model')->nullable();
+            $table->string('serie')->nullable();
+            $table->string('capability')->nullable();
+            $table->string('photos')->nullable();
+            $table->foreignId('client_id')
+                    ->nullable()
+                    ->constrained()
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('receptions');
+    }
+};
